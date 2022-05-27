6ec6aae248bc39e23eb475562915c0db5f30570f
Summer Etherington <summersce23@gmail.com>
2022-05-27 15:43:40 -0600

    app-2 finished

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      list: ['1', '2', '3']
+    }
+  }
+
+  render() {
+    let display = this.state.list.map((elem, index) => {
+      return <h2 key={index}>{elem}</h2>
+    })
+    return (
+      <div className="App">
+        {display}
+      </div>
+    );
+  }
 }
 
 export default App;
