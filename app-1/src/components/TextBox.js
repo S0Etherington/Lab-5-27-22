53a32ba5b0d04be17707a67b290e438e52bb82c4
Summer Etherington <summersce23@gmail.com>
2022-05-27 15:36:41 -0600

    app-1 complete

---
+++ app-1/src/components/TextBox.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// const handleChange = (e) => {
+    
+// }
+
+// const TextBox = () => {
+//   return (
+//     <input type='text' placeholder='enter words'></input>
+//   )
+// }
+
+// export default TextBox
